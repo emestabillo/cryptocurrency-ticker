51d51b7312a373e554bb0f4e5d9ef911c80aa4f5
Emmilie Estabillo <emsestabillo@gmail.com>
2019-04-12 08:24:34 -0400

    Add tickers component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Tickers from './components/Tickers';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Cryptocurrency Ticker</h2>
         </div>
+        <Tickers />
       </div>
     );
   }
